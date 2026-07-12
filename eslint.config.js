5ebc1d8ee43b626adaeb532a78e18d40a54592ad
tocque <tocque@sina.com>
2026-07-12 14:09:57 +0800

    chore(eslint): warn on unused variables

--- eslint.config.js
+++ eslint.config.js
@@ -54,7 +54,7 @@ export default tseslint.config(
         },
       }],
 
-      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/ban-ts-comment": "warn",
       "@typescript-eslint/no-empty-object-type": "warn",
